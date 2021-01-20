60116b5cbe45bc60cd119e40df5f000e9cd3c2da
EntropicSymmetry <55453966+EntropicSymmetry@users.noreply.github.com>
2021-01-20 14:28:27 +0800

    Update rasaxutils.py

--- rasaxutils.py
+++ rasaxutils.py
@@ -15,10 +15,6 @@ def getTest(message, response):
 	"Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
 	"Thoughts that you would be better off dead, or of hurting yourself in some way?"
 	]
-	def getFirstQuestion():
-		return {'text':questions[0], 'buttons':answers, 'type':"PHQuestions"}
-	def getQuestions():
-		return questions
 
 	answers = [
 	"Not at all",
@@ -41,6 +37,40 @@ def getTest(message, response):
 			except Exception as e:
 				pass
 		depSeverity = outcomes[(-27//sumTotal)]
-		return {'question':'None', 'buttons':'None', 'score':depSeverity}
+		return {'question':[], 'buttons':[], 'score':depSeverity}
 
 	return jsonify({'text':questions[questionIndex+1], 'buttons':possibleAnswers, 'type':'PHQuestions'})
+
+def getFirstQuestion():
+	questions = [
+	"Little interest or pleasure in doing things?",
+	"Feeling down, depressed, or hopeless?",
+	"Trouble falling or staying asleep, or sleeping too much?",
+	"Feeling tired or having little energy?",
+	"Poor appetite or overeating?",
+	"Feeling bad about yourself - or that you are a failure or have let yourself or your family down?",
+	"Trouble concentrating on things, such as reading the newspaper or watching television?",
+	"Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
+	"Thoughts that you would be better off dead, or of hurting yourself in some way?"
+	]
+	answers = [
+	"Not at all",
+	"Several days",
+	"More than half the days",
+	"Nearly every day"
+	]
+
+	return {'text':questions[0], 'buttons':answers, 'type':"PHQuestions"}
+def getQuestions():
+	questions = [
+	"Little interest or pleasure in doing things?",
+	"Feeling down, depressed, or hopeless?",
+	"Trouble falling or staying asleep, or sleeping too much?",
+	"Feeling tired or having little energy?",
+	"Poor appetite or overeating?",
+	"Feeling bad about yourself - or that you are a failure or have let yourself or your family down?",
+	"Trouble concentrating on things, such as reading the newspaper or watching television?",
+	"Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
+	"Thoughts that you would be better off dead, or of hurting yourself in some way?"
+	]
+	return questions
