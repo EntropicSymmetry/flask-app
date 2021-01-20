d8bb40f7276d480daf9f96e3020e3eb522cf0280
EntropicSymmetry <55453966+EntropicSymmetry@users.noreply.github.com>
2021-01-20 14:28:06 +0800

    Update application.py

--- application.py
+++ application.py
@@ -8,9 +8,11 @@ from flask import  request, redirect, url_for, flash, jsonify, Response, make_re
 from flask_cors import CORS, cross_origin
 from datetime import datetime
 import json
+from flask_ngrok import run_with_ngrok
 
 #Define app
 application = Flask(__name__)
+run_with_ngrok(application)
 
 #Add CORS
 cors = CORS(application)
@@ -52,7 +54,7 @@ def getMessage():
 	import requests
 	from requests.structures import CaseInsensitiveDict
 	import json
-	from rasaxutils import getTest
+	from rasaxutils import getTest, getFirstQuestion, getQuestions
 
 	senderID = data['sender']+str(datetime.today().strftime('%Y-%m-%d')).replace("-","")
 
@@ -72,10 +74,10 @@ def getMessage():
 	resp_to_return['type'] = 'message'
 	resp_to_return['buttons'] = []
 
-	metadata_url = "http://stella.unitedwecare.ca/api/conversations/"+"me"+"?format=full_conversation&since=1611116400.9448216&environment=production"
+	metadata_url = "http://stella.unitedwecare.ca/api/conversations/"+senderID+"?format=full_conversation&since=1611116400.9448216&environment=production"
 
 	if bool(data['questions']) == True:
-		if data['questions'][-1] in getTest.getQuestions():
+		if data['questions'][-1] in getQuestions():
 			resp_to_return = getTest(data['questions'], data['responses'])
 			resp_to_return['sender'] = data['sender']
 			return resp_to_return
@@ -83,10 +85,11 @@ def getMessage():
 	resp_metadata = requests.get(metadata_url, headers=headers)
 
 	print(resp_metadata.json())
+
 	if bool(resp_metadata.json()['latest_message']['intent'])==True:
 		myIntent = resp_metadata.json()['latest_message']['intent']['name']
-		if myIntent == 'utter_DepressionOne':
-			resp_to_return = getTest.getFirstQuestion()
+		if myIntent == 'Gen_Depression':
+			resp_to_return = getFirstQuestion()
 			resp_to_return['sender'] = data['sender']
 
 	print(resp_text.status_code)
@@ -95,4 +98,4 @@ def getMessage():
 
 
 if __name__ == '__main__':
-	application.run(debug=False, threaded=True)
+	application.run()
