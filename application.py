861039bf2e5314d4477fa89d8bc747c006457515
EntropicSymmetry <55453966+EntropicSymmetry@users.noreply.github.com>
2021-02-12 23:05:27 +0800

    Update application.py

--- application.py
+++ application.py
@@ -8,11 +8,9 @@ from flask import  request, redirect, url_for, flash, jsonify, Response, make_re
 from flask_cors import CORS, cross_origin
 from datetime import datetime
 import json
-from flask_ngrok import run_with_ngrok
 
 #Define app
 application = Flask(__name__)
-run_with_ngrok(application)
 
 #Add CORS
 cors = CORS(application)
@@ -67,20 +65,59 @@ def getMessage():
 
 	send_data = {"message":data['responses'][-1]}
 
-
-	resp_text = requests.post(url, headers=headers, data=json.dumps(send_data))
-	resp_to_return = resp_text.json()[0]
-	print(resp_to_return)
-	resp_to_return['type'] = 'message'
-	resp_to_return['buttons'] = []
+	try:
+		resp_text = requests.post(url, headers=headers, data=json.dumps(send_data))
+		resp_to_return = resp_text.json()[0]
+		print(resp_to_return)
+		resp_to_return['type'] = 'message'
+		resp_to_return['buttons'] = []
+	except Exception as e:
+		pass
 
 	metadata_url = "http://stella.unitedwecare.ca/api/conversations/"+senderID+"?format=full_conversation&since=1611116400.9448216&environment=production"
 
 	if bool(data['questions']) == True:
-		if data['questions'][-1] in getQuestions():
-			resp_to_return = getTest(data['questions'], data['responses'])
-			resp_to_return['sender'] = data['sender']
-			return resp_to_return
+		if data['questions'][-1] in getQuestions('PHQuestions') or data['questions'][-1] in getQuestions('RelationshipQuestions')  or data['questions'][-1] in getQuestions('Y-BOC') or  data['questions'][-1] in getQuestions('PHQ-15'):
+			
+			if data['questions'][-1] in getQuestions('RelationshipQuestions'):
+				resp_to_return = getTest(data['questions'], data['responses'], "RelationshipQuestions")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('PHQuestions'):
+				resp_to_return = getTest(data['questions'], data['responses'], "PHQuestions")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('PHQ-15'):
+				resp_to_return = getTest(data['questions'], data['responses'], "PHQ-15")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('Y-BOC'):
+				resp_to_return = getTest(data['questions'], data['responses'], "Y-BOC")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('GAD-7'):
+				resp_to_return = getTest(data['questions'], data['responses'], "GAD-7")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('Socrates'):
+				resp_to_return = getTest(data['questions'], data['responses'], "Socrates")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('PSS'):
+				resp_to_return = getTest(data['questions'], data['responses'], "PSS")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
+
+			elif data['questions'][-1] in getQuestions('SPIN'):
+				resp_to_return = getTest(data['questions'], data['responses'], "SPIN")
+				resp_to_return['sender'] = data['sender']
+				return resp_to_return
 
 	resp_metadata = requests.get(metadata_url, headers=headers)
 
@@ -89,7 +126,35 @@ def getMessage():
 	if bool(resp_metadata.json()['latest_message']['intent'])==True:
 		myIntent = resp_metadata.json()['latest_message']['intent']['name']
 		if myIntent == 'Gen_Depression':
-			resp_to_return = getFirstQuestion()
+			resp_to_return = getFirstQuestion('PHQuestions')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'Gen_RelationshipIssues':
+			resp_to_return = getFirstQuestion('RelationshipQuestions')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_OCD':
+			resp_to_return = getFirstQuestion('Y-BOC')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_StressBody':
+			resp_to_return = getFirstQuestion('PHQ-15')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_Anxious':
+			resp_to_return = getFirstQuestion('GAD-7')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_Addiction':
+			resp_to_return = getFirstQuestion('Socrates')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_StressMind':
+			resp_to_return = getFirstQuestion('PSS')
+			resp_to_return['sender'] = data['sender']
+
+		if myIntent == 'gen_StressMind':
+			resp_to_return = getFirstQuestion('PSS')
 			resp_to_return['sender'] = data['sender']
 
 	print(resp_text.status_code)
@@ -98,4 +163,4 @@ def getMessage():
 
 
 if __name__ == '__main__':
-	application.run()
+	application.run(debug=False, threaded=True)
