56e4572e14c0f0779bbc58f3711e0f7710c8d925
EntropicSymmetry <55453966+EntropicSymmetry@users.noreply.github.com>
2021-03-01 13:19:39 +0800

    Update updates.py

--- updates.py
+++ updates.py
@@ -8,18 +8,20 @@ def regexForDates(st):
 ^((((0[13578])|([13578])|(1[02]))[\/](([1-9])|([0-2][0-9])|(3[01])))|(((0[469])|([469])|(11))[\/](([1-9])|([0-2][0-9])|(30)))|((2|02)[\/](([1-9])|([0-2][0-9]))))[\/]\d{4}$|^\d{4}$"""
   return list(datefinder.find_dates(st))
 
-#Get datelist from text
-dateListRecord = regexForDates(str(resp_metadata.json()['latest_message']))
+def storeReminderDates(metadata, username):
+  
+  #Get datelist from text
+  dateListRecord = regexForDates(str(resp_metadata.json()['latest_message']))
 
-#Check if any dates were found
-if bool(dateListRecord) == True:
-  
-  #Get mongo client
-  import pymongo
-  client = pymongo.MongoClient("mongodb+srv://dbUser:deUserPassword@Stella@uwc-ca.7fijl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-  
-  #Insert each datetime in mongo
-  [client.Stella.KeyWords.update_one({"_id":username}, {"$push":{"DateList":FoundDate}}) for FoundDate in dateListRecord]
+  #Check if any dates were found
+  if bool(dateListRecord) == True:
+
+    #Get mongo client
+    import pymongo
+    client = pymongo.MongoClient("mongodb+srv://dbUser:deUserPassword@Stella@uwc-ca.7fijl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+    #Insert each datetime in mongo
+    [client.Stella.KeyWords.update_one({"_id":username}, {"$push":{"DateList":FoundDate}}) for FoundDate in dateListRecord]
   
 #Create a cron job function to get reminders:
 def getReminders():
@@ -35,8 +37,12 @@ def getReminders():
     #Put a try statement in case any errors are encountered while reading data
     try:
       for Storeddate in oneUser['DateList']:
+        
+        #Check if date is for tomrrow (can change this to anything by altering the delta)
         if StoredDate == datetime.date.today() + datetime.timedelta(days=1):
           storeThisUser = True
+   
+    #If there is a date, add the user to the reminder list.
     if storeThisUser == True:
       reminderList.append(oneUser)
       
@@ -44,4 +50,3 @@ def getReminders():
       pass
     
     return reminderList
-    
