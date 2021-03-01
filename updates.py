455c1af11d87603d3ed45c48409047f762ae400a
EntropicSymmetry <55453966+EntropicSymmetry@users.noreply.github.com>
2021-03-01 12:21:16 +0800

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,1 +1,9 @@
+def regexForDates(st):
+  import datefinder, import re
+  regexChecks = ["|/A_z?1_10/|"]
+  return list(datefinder.find_dates(st))
+
 dateListRecord = regexForDates(str(resp_metadata.json()['latest_message']))
+
+if bool(dateListRecord) == True:
+  
